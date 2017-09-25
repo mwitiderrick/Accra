00fa81293140a21860a156f454f8c1fd330c1185
Derrick Mwiti <mwitiderrick@gmail.com>
2017-09-25 10:19:14 +0000

    Made a few changes to the variables

--- accra.py
+++ accra.py
@@ -6,7 +6,7 @@ contact_details = {}
 
 def search_name(name_to_search):
     if name_to_search in contact_details.keys():
-        print(contact_details[name_to_be_search])
+        print(contact_details[name_to_search])
     else:
         print('Contact was not found')
 
@@ -19,16 +19,8 @@ while True:
         email = input('Please enter your email address: ')
         contact_details[name] = email
     if user_input == '2':
-        name_to_be_search = input('Please enter name to search: ')
-        search_name(name_to_be_search)
+        name_to_be_searched = input('Please enter name to search: ')
+        search_name(name_to_be_searched)
     if user_input == 'q':
         print(contact_details)
         sys.exit()
-
-
-
-
-
-
-
-
